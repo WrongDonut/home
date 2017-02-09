8e830a6abd0da562f101703129d5b843b463b334
WrongDonut <wrong.doughnut@gmail.com>
2017-02-09 08:41:21 -0500

    Update homeJS.js

--- homeJS.js
+++ homeJS.js
@@ -1,4 +1,4 @@
 window.onload = function(){
 	var links = document.getElementById("links");
-	links.innerHTML+=('<a href="homePage.html"><img class="head" src="Assets/favicon.png"/> Home</a>  <a href="music.html"><img class="head" src="Assets/music.png"/> Music</a>  <a href="ridiculous.html"><img class="head" src="Assets/ridiculous.png"/> Ridiculous</a>  <a href="personal.html"><img class="head" src="Assets/personal.png"/> Personal</a>');
+	links.innerHTML+=('<a href="index.html"><img class="head" src="Assets/favicon.png"/> Home</a>  <a href="music.html"><img class="head" src="Assets/music.png"/> Music</a>  <a href="ridiculous.html"><img class="head" src="Assets/ridiculous.png"/> Ridiculous</a>  <a href="personal.html"><img class="head" src="Assets/personal.png"/> Personal</a>');
 }
